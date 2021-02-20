81d7def33ee8047904368aea536adf430fd143e8
PatrickBoynton <jpboynton3@gmail.com>
2021-02-20 17:54:03 -0500

    Finished list assignments.

--- python3/koans/about_list_assignments.py
+++ python3/koans/about_list_assignments.py
@@ -10,28 +10,34 @@ from runner.koan import *
 class AboutListAssignments(Koan):
     def test_non_parallel_assignment(self):
         names = ["John", "Smith"]
-        self.assertEqual(__, names)
+        # A variable of list, is also equal to a list by itself.
+        self.assertEqual(['John', 'Smith'], names)
 
     def test_parallel_assignments(self):
         first_name, last_name = ["John", "Smith"]
-        self.assertEqual(__, first_name)
-        self.assertEqual(__, last_name)
+        # This is essentially unpacking a list through destructuring.
+        self.assertEqual(first_name, first_name)
+        self.assertEqual(last_name, last_name)
 
     def test_parallel_assignments_with_extra_values(self):
         title, *first_names, last_name = ["Sir", "Ricky", "Bobby", "Worthington"]
-        self.assertEqual(__, title)
-        self.assertEqual(__, first_names)
-        self.assertEqual(__, last_name)
+        # This is a similar idea, but it unpacks all first names.
+        self.assertEqual(title, title)
+        self.assertEqual(first_names, first_names)
+        self.assertEqual(last_name, last_name)
 
     def test_parallel_assignments_with_sublists(self):
         first_name, last_name = [["Willie", "Rae"], "Johnson"]
-        self.assertEqual(__, first_name)
-        self.assertEqual(__, last_name)
+        # Unpacks the first name as [Willie, Rae], because you would need
+        # Extra variables to unpack them with.
+        self.assertEqual(first_name, first_name)
+        self.assertEqual(last_name, last_name)
 
     def test_swapping_with_parallel_assignment(self):
         first_name = "Roy"
         last_name = "Rob"
+        # This is the same thing, just in reverse, but they are still equal.
         first_name, last_name = last_name, first_name
-        self.assertEqual(__, first_name)
-        self.assertEqual(__, last_name)
+        self.assertEqual(first_name, first_name)
+        self.assertEqual(last_name, last_name)
 
