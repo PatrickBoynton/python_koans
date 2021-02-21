2a74f5e9cba76e77f49b5784174e0ac713ca6958
PatrickBoynton <jpboynton3@gmail.com>
2021-02-20 22:07:18 -0500

    Finished true and false.

--- python3/koans/about_true_and_false.py
+++ python3/koans/about_true_and_false.py
@@ -12,32 +12,37 @@ class AboutTrueAndFalse(Koan):
             return 'false stuff'
 
     def test_true_is_treated_as_true(self):
-        self.assertEqual(__, self.truth_value(True))
+        self.assertEqual("true stuff", self.truth_value(True))
 
     def test_false_is_treated_as_false(self):
-        self.assertEqual(__, self.truth_value(False))
+        self.assertEqual("false stuff", self.truth_value(False))
 
+    # None is treated as false.
     def test_none_is_treated_as_false(self):
-        self.assertEqual(__, self.truth_value(None))
+        self.assertEqual("false stuff", self.truth_value(None))
 
+    # Like most languages 0 is treated as false.
     def test_zero_is_treated_as_false(self):
-        self.assertEqual(__, self.truth_value(0))
+        self.assertEqual("false stuff", self.truth_value(0))
 
+    # Empty collections are false.
     def test_empty_collections_are_treated_as_false(self):
-        self.assertEqual(__, self.truth_value([]))
-        self.assertEqual(__, self.truth_value(()))
-        self.assertEqual(__, self.truth_value({}))
-        self.assertEqual(__, self.truth_value(set()))
+        self.assertEqual("false stuff", self.truth_value([]))
+        self.assertEqual("false stuff", self.truth_value(()))
+        self.assertEqual("false stuff", self.truth_value({}))
+        self.assertEqual("false stuff", self.truth_value(set()))
 
+    # In most languages an empty string is falsy.
     def test_blank_strings_are_treated_as_false(self):
-        self.assertEqual(__, self.truth_value(""))
+        self.assertEqual("false stuff", self.truth_value(""))
 
+    # Anything with a value (a space is a value) is treated as true.
     def test_everything_else_is_treated_as_true(self):
-        self.assertEqual(__, self.truth_value(1))
-        self.assertEqual(__, self.truth_value([0]))
-        self.assertEqual(__, self.truth_value((0,)))
+        self.assertEqual("true stuff", self.truth_value(1))
+        self.assertEqual("true stuff", self.truth_value([0]))
+        self.assertEqual("true stuff", self.truth_value((0,)))
         self.assertEqual(
-            __,
+            "true stuff",
             self.truth_value("Python is named after Monty Python"))
-        self.assertEqual(__, self.truth_value(' '))
-        self.assertEqual(__, self.truth_value('0'))
+        self.assertEqual("true stuff", self.truth_value(' '))
+        self.assertEqual("true stuff", self.truth_value('0'))
